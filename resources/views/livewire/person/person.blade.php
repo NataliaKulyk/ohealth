0aeeb952c7d14d86a2155d598aece90755f03fa4
Natalia <nataliakulyk123@gmail.com>
2026-06-26 16:24:18 +0300

    feat: add unidentified patient creation form

--- resources/views/livewire/person/person.blade.php
+++ resources/views/livewire/person/person.blade.php
@@ -1,7 +1,7 @@
 @use('App\Models\Person\PersonRequest')
 @use('App\Livewire\Person\PersonUpdate')
 
-<div>
+<div x-data="{ showUnidentifiedPatientModal: false }">
     <x-header-navigation class="breadcrumb-form">
         <x-slot name="title">{{ __('patients.add_patient') }}</x-slot>
     </x-header-navigation>
@@ -9,36 +9,59 @@
     @if($viewState === 'default')
         <section wire:key="{{ $viewState }}" class="section-form shift-content">
             <form class="form" wire:key="patient-form-{{ $formKey }}">
+                @include('livewire.person.parts.patient-type')
                 @include('livewire.person.parts.person')
-                @include('livewire.person.parts.documents')
-                @include('livewire.person.parts.identity')
-                @include('livewire.person.parts.contact-data')
-                @include('livewire.person.parts.addresses')
-                @include('livewire.person.parts.emergency-contact')
-                @include('livewire.person.parts.incapacitated')
-                @if(!$this instanceof PersonUpdate)
-                    @include('livewire.person.parts.authentication-methods')
+
+                @if(($form->person['patientType'] ?? 'identified') === 'unidentified')
+                    @include('livewire.person.parts.unidentified-contact-person')
                 @endif
 
-                <div class="flex xl:flex-row gap-6 justify-between items-center">
-                    <a href="{{ route('persons.index', [legalEntity()]) }}" class="button-minor">
-                        {{ __('forms.back') }}
-                    </a>
+                @if(($form->person['patientType'] ?? 'identified') === 'identified')
+                    @include('livewire.person.parts.documents')
+                    @include('livewire.person.parts.identity')
+                    @include('livewire.person.parts.contact-data')
+                    @include('livewire.person.parts.addresses')
+                    @include('livewire.person.parts.emergency-contact')
+                    @include('livewire.person.parts.incapacitated')
+                    @if(!$this instanceof PersonUpdate)
+                        @include('livewire.person.parts.authentication-methods')
+                    @endif
+                @endif
 
+                <div class="flex flex-wrap gap-4 items-center">
                     @if($this instanceof PersonUpdate)
+                        <a href="{{ route('persons.index', [legalEntity()]) }}" class="button-minor">
+                            {{ __('forms.back') }}
+                        </a>
+
                         @can('create', PersonRequest::class)
                             <button type="submit" wire:click.prevent="openAuthMethodModal" class="button-primary">
                                 {{ __('forms.update_data') }}
                             </button>
                         @endcan
                     @else
+                        <a href="{{ route('persons.index', [legalEntity()]) }}" class="button-primary-outline-red">
+                            {{ __('forms.delete') }}
+                        </a>
+
                         @can('create', PersonRequest::class)
-                            <button type="submit" wire:click.prevent="createLocally" class="button-primary-outline">
-                                {{ __('forms.save') }}
-                            </button>
-                            <button type="submit" wire:click.prevent="create" class="button-primary">
-                                {{ __('forms.save_and_send') }}
-                            </button>
+                            @if(($form->person['patientType'] ?? 'identified') === 'unidentified')
+                                <button type="button" @click.prevent="window.location.href = '{{ route('persons.index', [legalEntity()]) }}'" class="button-primary-outline flex items-center gap-2">
+                                    @icon('archive', 'w-4 h-4')
+                                    {{ __('forms.save') }}
+                                </button>
+                                <button type="button" @click.prevent="showUnidentifiedPatientModal = true" class="button-primary">
+                                    {{ __('forms.create') }}
+                                </button>
+                            @else
+                                <button type="submit" wire:click.prevent="createLocally" class="button-primary-outline flex items-center gap-2">
+                                    @icon('archive', 'w-4 h-4')
+                                    {{ __('forms.save') }}
+                                </button>
+                                <button type="submit" wire:click.prevent="create" class="button-primary">
+                                    {{ __('forms.create') }}
+                                </button>
+                            @endif
                         @endcan
                     @endif
                 </div>
@@ -64,6 +87,8 @@
     @if($showLeafletModal)
         @include('livewire.person.parts.modals.leaflet')
     @endif
+
+    @include('livewire.person.parts.modals.unidentified-warning')
 
     @can('create', PersonRequest::class)
         <x-signature-modal method="sign" />
